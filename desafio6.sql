cc7f0342ab4dafb9168665d6b58582d176a2911a
Bruno Cabral <bruno.cabral.silva2018@gmail.com>
2022-08-18 20:21:42 -0300

    Requisitos do 1 ao 9

--- desafio6.sql
+++ desafio6.sql
@@ -4,4 +4,5 @@ SELECT
     ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
     SUM(p.plan_value) AS faturamento_total
 FROM SpotifyClone.plan AS p
-INNER JOIN SpotifyClone.users AS u;
+INNER JOIN SpotifyClone.users AS u
+ON u.plan_id = p.plan_id;
--- desafio9.sql
+++ desafio9.sql
@@ -1,3 +1,5 @@
-SELECT COUNT(*) AS quantidade_musicas_no_historico
-FROM SpotifyClone.historic
-WHERE users_id = 1;
+SELECT COUNT(h.song_id) AS quantidade_musicas_no_historico
+FROM SpotifyClone.historic AS h
+INNER JOIN SpotifyClone.users AS u
+ON h.users_id = u.users_id
+WHERE u.users_name = "Barbara Liskov";
